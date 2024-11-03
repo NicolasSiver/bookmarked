b3a448ca2be39b2bbf344d1cb99fdb4d44657160
Nicolas Siver <nicolas@siver.im>
2024-11-02 22:29:00 -0400

    add initial structure for controller

---
+++ src/controller/main-controller.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { createInitState, createNewStore } from '../model/store';
+
+export class MainController {
+    constructor() {
+        this.store = createNewStore(createInitState());
+    }
+
+    init() {
+        console.log('Expansion is initialising...');
+
+        this.render();
+    }
+
+    render() {
+        let root = createRoot(document.getElementsByClassName('root')[0]);
+        root.render(
+            <Provider store={this.store}>
+
+            </Provider>
+        );
+    }
+}
