0bb5446169b9fd760f5784f3f23a63da8857a563
Nicolas Siver <nicolas@siver.im>
2025-05-22 14:22:02 -0400

    add initial mock data

--- src/model/store.js
+++ src/model/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import CollectionsSlice from './collections-slice';
+import CollectionsSlice from "./collections-slice";
+import ItemsSlice from "./items-slice";
 import * as Modes from "./modes";
 
 export function createInitState() {
@@ -11,7 +12,20 @@ export function createInitState() {
             { id: "testId2", name: "Extended" },
             { id: "testId3", name: "Last" }
         ],
-        items: [],
+        items: {
+            // TODO Remove after very initial rounds of development
+            testId1: [
+                { id: "itemId1", title: "Test Title 1", description: "desc temp", url: "https://some.com" },
+                { id: "itemId2", title: "Test Title 2", description: "desc temp", url: "https://some2.com" },
+                { id: "itemId3", title: "Test Title 3", description: "desc temp", url: "https://some3.com" },
+                { id: "itemId4", title: "Test Title 4", description: "desc temp", url: "https://some4.com" },
+                { id: "itemId5", title: "Test Title 5", description: "desc temp", url: "https://some5.com" },
+                { id: "itemId6", title: "Test Title 6", description: "desc temp", url: "https://some6.com" },
+                { id: "itemId7", title: "Test Title 7", description: "desc temp", url: "https://some7.com" },
+                { id: "itemId8", title: "Test Title 8", description: "desc temp", url: "https://some8.com" },
+                { id: "itemId9", title: "Test Title 9", description: "desc temp", url: "https://some9.com" }
+            ]
+        },
         mode: Modes.VIEW
     };
 }
@@ -20,7 +34,8 @@ export function createNewStore(initState) {
     return configureStore({
         preloadedState: initState,
         reducer: {
-            collections: CollectionsSlice
+            collections: CollectionsSlice,
+            items: ItemsSlice
         }
     });
 }
