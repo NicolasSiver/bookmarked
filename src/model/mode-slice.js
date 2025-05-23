161c709ef44830c1d4fe56f2b1ec4ad6cedf5b56
Nicolas Siver <nicolas@siver.im>
2025-05-23 19:52:53 -0400

    add mode slice

---
+++ src/model/mode-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import * as Modes from './modes';
+
+let initialState = Modes.VIEW;
+
+let modeSlice = createSlice({
+    name: 'mode',
+    initialState,
+    reducers: {
+        changeMode(state, action) {
+            return action.payload;
+        }
+    }
+});
+
+export const { changeMode } = modeSlice.actions;
+export default modeSlice.reducer;
