58510fcbe2f861e64b41b31d531529fd7d66624e
Nicolas Siver <nicolas@siver.im>
2025-05-28 19:37:12 -0400

    change collection splice to refer to the array

--- src/model/collections-slice.js
+++ src/model/collections-slice.js
@@ -6,11 +6,17 @@ let collectionsSlice = createSlice({
     name: 'collections',
     initialState,
     reducers: {
-        add(state, action) {
-            state.collections.push(action.payload);
+        addCollection(state, action) {
+            state.push(action.payload);
+        },
+
+        shiftColleciton(state, action) {
+            const { fromIndex, toIndex } = action.payload;
+            const collection = state.splice(fromIndex, 1)[0];
+            state.splice(toIndex, 0, collection);
         }
     }
 });
 
-export const { add } = collectionsSlice.actions;
+export const { addCollection, shiftColleciton } = collectionsSlice.actions;
 export default collectionsSlice.reducer;
