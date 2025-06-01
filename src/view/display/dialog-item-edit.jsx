a46cc71f6ace64675e639aa3145b7f5413d6781d
Nicolas Siver <nicolas@siver.im>
2025-05-31 20:30:54 -0400

    add essential ui with collections

--- src/view/display/dialog-item-edit.jsx
+++ src/view/display/dialog-item-edit.jsx
@@ -1,19 +1,42 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 
 import { getCollectionById } from "../../model/selector/get-collection-by-id";
 import { getCollectionItemsById } from "../../model/selector/get-collection-items-by-id";
+import { getCollections } from "../../model/selectors";
 
 export const DialogItemEdit = (props) => {
     const { collectionId, itemId } = props.dialogContext
     const collection = useSelector(getCollectionById(collectionId));
+    const collections = useSelector(getCollections);
     const items = useSelector(getCollectionItemsById(collectionId));
     const itemIndex = items.findIndex(item => item.id === itemId);
     const item = items[itemIndex];
 
     const dialogCloseCallback = () => {
         props.closeDialog();
+    };
+
+    // TODO Consider to move Delete button to the bottom of the dialog to the dedicated section
+
+    const createCollectionSelect = () => {
+        return (
+            <FormControl fullWidth margin="dense">
+                <InputLabel id="collection-select-label">Parent collection</InputLabel>
+                <Select
+                    labelId="collection-select-label"
+                    value={collectionId}
+                    label="Parent collection"
+                    onChange={() => undefined}>
+                    {collections.map(collection => (
+                        <MenuItem key={collection.id} value={collection.id}>
+                            {collection.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        );
     };
 
     return (
@@ -25,11 +48,33 @@ export const DialogItemEdit = (props) => {
                 <DialogTitle>Edit: "{item.title}"</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        TODO Text
+                        TODO Text?
                     </DialogContentText>
+                    <TextField
+                        required
+                        margin="dense"
+                        label="Title"
+                        fullWidth
+                        value={item.title}
+                        onChange={() => undefined} />
+                    <TextField
+                        margin="dense"
+                        label="Description"
+                        fullWidth
+                        value={item.description}
+                        onChange={() => undefined} />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="URL"
+                        fullWidth
+                        type="url"
+                        value={item.url}
+                        onChange={() => undefined} />
+                    {createCollectionSelect()}
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={() => undefined}>Delete</Button>
+                    <Button color="error" onClick={() => undefined}>Delete</Button>
                     <Button onClick={dialogCloseCallback}>Close</Button>
                 </DialogActions>
             </Dialog>
