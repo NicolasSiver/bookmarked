ca50c99bc75ffa7af71fcb67ba4c1f9c634c12e2
Nicolas Siver <nicolas@siver.im>
2025-05-31 20:56:52 -0400

    add item enum

---
+++ src/model/item-properties.js
@@ -0,0 +1,34 @@
+/**
+ * Long description of the item.
+ * @type {string}
+ * @constant
+ */
+export const DESCRIPTION = 'description';
+
+/**
+ * Parent collection of the item.
+ * @type {string}
+ * @constant
+ */
+export const PARENT = 'parent';
+
+/**
+ * Position of the item in the collection.
+ * @type {number}
+ * @constant
+ */
+export const POSITION = 'position';
+
+/**
+ * Short description of the item.
+ * @type {string}
+ * @constant
+ */
+export const TITLE = 'title';
+
+/**
+ * Fully qualified URL of the item.
+ * @type {string}
+ * @constant
+ */
+export const URL = 'url';
