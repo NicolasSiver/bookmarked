56855515694dc3f63dfaea2b77fe2cd511ad5530
Nicolas Siver <nicolas@siver.im>
2025-06-04 22:09:13 -0400

    change to prevent persistence for hydration actions

--- src/controller/persist-middleware.js
+++ src/controller/persist-middleware.js
@@ -11,8 +11,12 @@ export class PersistMiddleware {
         return store => next => action => {
             const result = next(action);
 
-            // Only persist if collections or items changed
-            if (action.type.startsWith(`${collectionsSlice.name}/`) || action.type.startsWith(`${itemsSlice.name}/`)) {
+            // Only persist if collections or items changed, and exclude any action containing 'hydrate'
+            const isCollectionAction = action.type.startsWith(`${collectionsSlice.name}/`);
+            const isItemAction = action.type.startsWith(`${itemsSlice.name}/`);
+            const isHydrationAction = action.type.includes('hydrate');
+
+            if ((isCollectionAction || isItemAction) && isHydrationAction === false) {
                 console.log(`Persisting state for action: ${action.type}`);
 
                 this.storage.setValue(collectionsSlice.name, store.getState()[collectionsSlice.name]);
