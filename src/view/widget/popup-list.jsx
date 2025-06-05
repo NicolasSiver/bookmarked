8995bc9c9aba25b1535f4a39d13a2ac9cb5aea3e
Nicolas Siver <nicolas@siver.im>
2025-06-05 19:00:54 -0400

    change hint message to include extension page

--- src/view/widget/popup-list.jsx
+++ src/view/widget/popup-list.jsx
@@ -2,18 +2,29 @@ import { Paper, Stack } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 
+import * as Constants from "../../model/constants";
 import { PopupItem } from "./popup-item";
-import { getCollections } from "../../model/selectors";
+import { getCollections, getTabUrl } from "../../model/selectors";
 
 export const PopupList = props => {
     const collections = useSelector(getCollections);
+    const tabUrl = useSelector(getTabUrl);
     const collectionCount = collections.length;
+    const extensionPage = tabUrl === Constants.NEW_TAB_URL;
 
     const createHint = () => {
+        let message;
+
+        if (extensionPage === true) {
+            message = "This is a new tab page. Switch to the tab you want to save and click the action button again.";
+        } else if (collectionCount === 0) {
+            message = "You have no collections yet. Please open the extension and create your first collection.";
+        }           
+
         return (
             <div className="popup-list__hint">
                 <Paper elevation={2} sx={{ px: 4, py: 2 }}>
-                    Please open the extension and create your first collection.
+                    {message}
                 </Paper>
             </div>
         );
@@ -38,7 +49,7 @@ export const PopupList = props => {
     return (
         <div className="popup-list">
             <Stack spacing={2} sx={{ px: 2, py: 1 }}>
-                {collectionCount === 0 ? createHint() : getCollectionItems()}
+                {collectionCount === 0 || extensionPage === true ? createHint() : getCollectionItems()}
             </Stack>
         </div>
     );
