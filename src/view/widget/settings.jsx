cb47c054a4773536a390d7fd5a9c185e759333b0
Nicolas Siver <nicolas@siver.im>
2025-06-13 19:49:13 -0400

    add basic setting options

--- src/view/widget/settings.jsx
+++ src/view/widget/settings.jsx
@@ -1,4 +1,4 @@
-import { Box, Drawer, LinearProgress, Link } from "@mui/material";
+import { Box, Drawer, FormControl, FormControlLabel, FormLabel, LinearProgress, Link, Stack, Switch, Typography } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 
@@ -20,12 +20,23 @@ export const Settings = (props) => {
                 open={settingsOpen}
                 onClose={toggleSettings}>
                 <div className="settings__content">
-                    <Box sx={{ display: 'flex', alignItems: 'center'}}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <div>Version: <span className="settings__content-value">{getVersion()}</span></div>
-                        <Link href="https://github.com/NicolasSiver/bookmarked/releases" sx={{px: '1rem'}}>Changelog</Link>
+                        <Link href="https://github.com/NicolasSiver/bookmarked/releases" sx={{ px: '1rem' }}>Changelog</Link>
                     </Box>
                     <p>Storage usage: <span className="settings__content-value">{Number(storageQuota).toFixed(1)}%</span></p>
                     <LinearProgress variant="determinate" value={storageQuota} />
+                    <Typography variant="h6" sx={{ my: 2 }}>General</Typography>
+                    <FormControl component="fieldset" variant="standard">
+                        <FormLabel>Theme</FormLabel>
+                    </FormControl>
+                    <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+                        <Typography>Light</Typography>
+                        <Switch />
+                        <Typography>Dark</Typography>
+                    </Stack>
+                    <Typography variant="h6" sx={{ my: 2 }}>View</Typography>
+                    TODO
                 </div>
             </Drawer>
         </div>
