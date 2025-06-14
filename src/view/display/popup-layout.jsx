af0eab14a6078b185588549c787ea93c9b90f66f
Nicolas Siver <nicolas@siver.im>
2025-06-13 21:02:10 -0400

    add theme switch for popup

--- src/view/display/popup-layout.jsx
+++ src/view/display/popup-layout.jsx
@@ -1,16 +1,22 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
+import { getMuiTheme } from "../../util/get-mui-theme";
 import { PopupList } from "../widget/popup-list";
+import { getSettingsMode } from "../../model/selectors";
 
 export const PopupLayout = props => {
+    const mode = useSelector(getSettingsMode);
 
     return (
         <Container disableGutters={true} maxWidth={false}>
-            <div className="popup-layout">
-                <CssBaseline enableColorScheme />
-                <PopupList {...props} />
-            </div>
+            <ThemeProvider theme={getMuiTheme(mode)}>
+                <div className="popup-layout">
+                    <CssBaseline enableColorScheme />
+                    <PopupList {...props} />
+                </div>
+            </ThemeProvider>
         </Container>
     );
 };
