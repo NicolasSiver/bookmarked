e38f2c25f9bd4e54759c70cba189e8d128a4cc18
Nicolas Siver <nicolas@siver.im>
2025-06-20 22:01:44 -0400

    change search bar to have styles outside of the render loop

--- src/view/widget/search-bar.jsx
+++ src/view/widget/search-bar.jsx
@@ -6,47 +6,53 @@ import { alpha, styled } from '@mui/material/styles';
 
 import { getSearchQuery } from '../../model/selectors';
 
+const StyledBackground = styled('div')(({ theme }) => ({
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    borderRadius: theme.shape.borderRadius,
+    marginLeft: 0,
+    position: 'relative',
+    width: '100%',
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto'
+    }
+}));
+
+const StyledIcon = styled('div')(({ theme }) => ({
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    padding: theme.spacing(0, 2),
+    pointerEvents: 'none',
+    position: 'absolute'
+}));
+
+const StyledInput = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            }
+        }
+    },
+}));
+
 export const SearchBar = props => {
     const query = useSelector(getSearchQuery);
 
     const queryCallback = event => {
         props.changeSearchQuery(event.target.value);
     };
-
-    const StyledBackground = styled('div')(({ theme }) => ({
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        borderRadius: theme.shape.borderRadius,
-        marginLeft: 0,
-        position: 'relative',
-        width: '100%',
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto'
-        }
-    }));
-
-    const StyledIcon = styled('div')(({ theme }) => ({
-        alignItems: 'center',
-        display: 'flex',
-        height: '100%',
-        justifyContent: 'center',
-        padding: theme.spacing(0, 2),
-        pointerEvents: 'none',
-        position: 'absolute'
-    }));
-
-    const StyledInput = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        width: '100%',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 1, 1, 0),
-            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-            width: '20ch'
-        },
-    }));
 
     return (
         <div className="search-bar">
