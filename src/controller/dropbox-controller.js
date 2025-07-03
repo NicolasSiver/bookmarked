9e767d4be8f2c91cb2862f7dfe8cd0156d17198d
Nicolas Siver <nicolas@siver.im>
2025-07-02 20:01:24 -0400

    add dropbox listeners

--- src/controller/dropbox-controller.js
+++ src/controller/dropbox-controller.js
@@ -1,14 +1,72 @@
 import { DropboxAuth } from "dropbox";
+import debounce from "lodash/debounce";
+import { isAnyOf } from "@reduxjs/toolkit";
 
+import { addCollection, changeCollectionName, deleteCollection, shiftCollection } from "../model/collections-slice";
 import * as Constants from "../model/constants";
 import { resetAuthorizationCodeFlow, resetDropboxSync, setCodeVerifier, setDropboxError, setRefreshToken } from "../model/dropbox-sync-slice";
+import { addItem, changeItemDescription, changeItemOrder, changeItemParent, changeItemTitle, changeItemUrl, deleteItem, deleteItemsByCollectionId } from "../model/items-slice";
 import { getDropboxSyncCodeChallenge, getDropboxSyncCodeVerifier, getDropboxSyncRefreshToken } from "../model/selectors";
+import { addSpace, changeCollectionSpaces, changeSpaceName, removeSpace, shiftSpace } from "../model/spaces-slice";
 import { StorageDocument } from "../util/storage-document";
 
 export class DropboxController {
     constructor() {
         this.dbxAuth = null;
         this.store = null;
+
+        this.collectionsEvent = isAnyOf(addCollection, changeCollectionName, deleteCollection, shiftCollection);
+        this.itemsEvent = isAnyOf(addItem, changeItemDescription, changeItemOrder, changeItemParent, changeItemTitle, changeItemUrl, deleteItem, deleteItemsByCollectionId);
+        this.spacesEvent = isAnyOf(addSpace, changeCollectionSpaces, changeSpaceName, removeSpace, shiftSpace);
+    }
+
+    addListeners(listenerMiddleware) {
+        this.listenerMiddleware = listenerMiddleware;
+
+        this.listenerMiddleware.startListening({
+            predicate: (action, currentState, previousState) => {
+                return getDropboxSyncRefreshToken(currentState) !== getDropboxSyncRefreshToken(previousState);
+            },
+            effect: (action, listenerApi) => {
+                console.log('Dropbox refresh token changed...');
+
+                if (this.isConnected() === true) {
+                    this.addStateListeners();
+                    // this.updateRemote();
+                } else {
+                    // If not connected, remove all state listeners
+                    console.log('Dropbox is disconnected, removing state listeners...');
+                    this.removeStateListeners();
+                }
+            }
+        });
+    }
+
+    addStateListeners() {
+        const debouncedUpdateRemote = debounce(() => {
+            this.updateRemote();
+        }, 8000, { maxWait: 16000 });
+
+        this.listenerMiddleware.startListening({
+            matcher: this.collectionsEvent,
+            effect: (action, listenerApi) => {
+                debouncedUpdateRemote();
+            }
+        });
+
+        this.listenerMiddleware.startListening({
+            matcher: this.itemsEvent,
+            effect: (action, listenerApi) => {
+                debouncedUpdateRemote();
+            }
+        });
+
+        this.listenerMiddleware.startListening({
+            matcher: this.spacesEvent,
+            effect: (action, listenerApi) => {
+                debouncedUpdateRemote();
+            }
+        });
     }
 
     auth() {
@@ -51,9 +109,21 @@ export class DropboxController {
         return refreshToken !== null && refreshToken.length > 0;
     }
 
+    removeStateListeners() {
+        console.log('Removing all state listeners...');
+
+        this.listenerMiddleware.stopListening({ matcher: this.collectionsEvent });
+        this.listenerMiddleware.stopListening({ matcher: this.itemsEvent });
+        this.listenerMiddleware.stopListening({ matcher: this.spacesEvent });
+    }
+
     revoke() {
         this.dbxAuth = null;
         this.store.dispatch(resetDropboxSync());
+    }
+
+    updateRemote() {
+        console.log('Updating remote Dropbox JSON file...');
     }
 
     verifyCodeChallenge() {
